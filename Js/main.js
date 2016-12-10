55e4b67a9186fa207148dbdb0483b44e4827ab25
Morarasu ALexandru <morarasu_alexandru@yahoo.com>
2016-12-10 18:32:54 +0200

    small bug fix and removed old code

--- Js/main.js
+++ Js/main.js
@@ -1,46 +1,8 @@
 (function() {
 	let target = document.getElementById('main');
-
-	let nav = document.getElementById('headerListc');
-	// let link1 = document.getElementById('link1');
-	// let link2 = document.getElementById('link2');
-	// let link3 = document.getElementById('link3');
-
-	// let page1 = '<div class="mainPage mainPage1"><h2 class="title">Content one</h2></div>';
-	// let page2 = '<div class="mainPage mainPage2"><h2 class="title">Content two</h2></div>';
-	// let page3 = '<div class="mainPage mainPage3"><h2 class="title">Content three</h2></div>';
+	let nav = document.getElementById('headerList');
 
 	nav.addEventListener('click', toggleContent);
-
-
-	// function someName(e){
-
-		// link1.classList.remove('active');
-		// link2.classList.remove('active');
-		// link3.classList.remove('active');
-
-		// if ( e.target.matches('#link1')) {
-		// 	link1.classList.add('active');
-		// 	while(target.firstChild) {
-		// 		target.removeChild(target.firstChild);
-		// 	}
-		// 	target.insertAdjacentHTML('afterbegin', page1);
-		// }
-		// else if (e.target.matches('#link2')) {
-		// 	link2.classList.add('active');
-		// 	while(target.firstChild) {
-		// 		target.removeChild(target.firstChild);
-		// 	}
-		// 	target.insertAdjacentHTML('afterbegin', page2);
-		// }
-		// else if (e.target.matches('#link3')) {
-		// 	link3.classList.add('active');
-		// 	while(target.firstChild) {
-		// 		target.removeChild(target.firstChild);
-		// 	}
-		// 	target.insertAdjacentHTML('afterbegin', page3);
-		// }
-	// });
 
 	function toggleContent(e) {
 		let mappedIndex = mapDataIndexToIndexNumber(e.target.dataset.index);
@@ -48,7 +10,7 @@
 		let elements = getElements(3, 'link');
 		removeActiveClassesFromAllSiblings(elements, classToRemove);
 
-		let content = `<div class="mainPage mainPage1"><h2 class="title">Content ${mappedIndex}</h2></div>`;
+		let content = `<div class="mainPage mainPage${mappedIndex}"><h2 class="title">Content ${mappedIndex}</h2></div>`;
 		let link = document.getElementById(`link${mappedIndex}`);
 
 		link.classList.add('active');
@@ -67,7 +29,7 @@
 	function getElements(totalElements, idStub) {
 		let arr = [];
 		for  (let i = 0; i < totalElements; i++ ) {
-			arr.push(document.getElementById(`${idStub}${i}`));
+			arr.push(document.getElementById(`${idStub}${i + 1}`));
 		}
 		return arr;
 	}
