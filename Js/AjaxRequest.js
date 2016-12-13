24840901d1442a094aa836982e24b7d05e6e2f76
alexandru.morarasu <alexandru.morarasu@everymatrix.com>
2016-12-13 18:18:56 +0200

    work in progress

--- Js/AjaxRequest.js
+++ Js/AjaxRequest.js
@@ -2,52 +2,20 @@
 	let nav = document.getElementById('headerList');
 	let main = document.getElementById('main');
 
-	function readTextFile(file)
-	{
-		let rawFile = new XMLHttpRequest();
-		rawFile.open("GET", file, false);
-		rawFile.onreadystatechange = function ()
-		{
-			if(rawFile.readyState === 4)
-			{
-				if(rawFile.status === 200 || rawFile.status == 0)
-				{
-					let allText = rawFile.responseText;
-					alert(allText);
-				}
-			}
-		};
-		rawFile.send(null);
-	}
+	let content1 =  getLocalElement('http://localhost:8080/Content/content1.html');
+	let content2 =  getLocalElement('http://localhost:8080/Content/content2.html');
+	let content3 =  getLocalElement('http://localhost:8080/Content/content3.html');
 
-	console.log(readTextFile('Js/ceva.ext'))
+	function getLocalElement(path) {
+
+		fetch(path).then(function (request) {
+			return request.text()
+		}).then((response) => {
+			console.log (response);
+			return response
+		});
+		return response;
+	}
 
 
 })();
-
-/*
-
-let mainElements = main.childElementCount;
-
-nav.addEventListener('click', toggleContent);
-
-function toggleContent(e) {
-	let mappedIndex = mapDataIndexToIndexNumber(e.target.dataset.index);
-	for (let i = 0; i < mainElements; i++) {
-		main.children[i].classList.remove('visible');
-	}
-	main.children[mappedIndex - 1].classList += ' visible';
-}
-
-function mapDataIndexToIndexNumber(tabIndex) {
-	let dataValues = constructDataValues();
-	return dataValues.indexOf(tabIndex) + 1;
-}
-
-function constructDataValues() {
-	arr = [];
-	for (let i= 0; i < mainElements; i++) {
-		arr.push(nav.children[i].dataset.index);
-	}
-	return arr;
-} */
