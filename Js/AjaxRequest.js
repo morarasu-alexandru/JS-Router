8ff18e40a63fd801288715a3548af9141bac674f
Morarasu ALexandru <morarasu_alexandru@yahoo.com>
2016-12-14 23:29:27 +0200

    work in progress 3

--- Js/AjaxRequest.js
+++ Js/AjaxRequest.js
@@ -1,22 +1,49 @@
 (function () {
 	let nav = document.getElementById('headerList');
 	let main = document.getElementById('main');
+	let navElements = nav.childElementCount;
 
-	let content1 =  getLocalElement('http://localhost:8080/Content/content1.html');
-	let content2 =  getLocalElement('http://localhost:8080/Content/content2.html');
-	let content3 =  getLocalElement('http://localhost:8080/Content/content3.html');
+	fetch('http://localhost:8080/Content/content1.html').then(function (request) {
+		return request.text()
+	}).then((response) => {
+		page1 = response;
+	});
+	fetch('http://localhost:8080/Content/content2.html').then(function (request) {
+		return request.text()
+	}).then((response) => {
+		page2 = response;
+	});
+	fetch('http://localhost:8080/Content/content3.html').then(function (request) {
+		return request.text()
+	}).then((response) => {
+		page3 = response;
+	});
 
-	function getLocalElement(path) {
+	nav.addEventListener('click', toggleContent);
 
-		fetch(path).then(function (request) {
-			return request.text()
-		}).then((response) => {
-			console.log (response);
-			return response
-		});
+	function toggleContent(e) {
+
+		let mappedIndex = mapDataIndexToIndexNumber(e.target.dataset.index);
+		for (let i = 0; i < navElements; i++) {
+			nav.children[i].classList.remove('active');
+		}
+		e.target.classList.add('active');
+		console.log(page1);
+		main.insertAdjacentHTML('afterbegin', page1);
 	}
 
 
+	function mapDataIndexToIndexNumber(tabIndex) {
+		let dataValues = constructDataValues();
+		return dataValues.indexOf(tabIndex) + 1;
+	}
 
+	function constructDataValues() {
+		arr = [];
+		for (let i= 0; i < nav; i++) {
+			arr.push(nav.children[i].dataset.index);
+		}
+		return arr;
+	}
 
 })();
